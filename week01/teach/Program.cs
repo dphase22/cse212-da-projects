0dd8b359c8650b700a8ca01b8fec4bca5f644c46
Demetrio Aris <gon21010@byui.edu>
2023-10-31 22:18:57 -0500

    Complete W01 Individual Activity

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -9,8 +9,8 @@
  */
 
 
-Console.WriteLine("\n======================\nDivisors\n======================");
-Divisors.Run();
+// Console.WriteLine("\n======================\nDivisors\n======================");
+// Divisors.Run();
 
 Console.WriteLine("\n======================\nArray Selector\n======================");
 ArraySelector.Run();
@@ -22,8 +22,8 @@ ArraySelector.Run();
 
 
 // Uncomment and run as you get to the solution part
-// Console.WriteLine("\n======================\nDivisors Solution\n======================");
-// DivisorsSolution.Run();
+//Console.WriteLine("\n======================\nDivisors Solution\n======================");
+//DivisorsSolution.Run();
 //
-// Console.WriteLine("\n======================\nArray Selector Solution\n======================");
-// ArraySelectorSolution.Run();
+Console.WriteLine("\n======================\nArray Selector Solution\n======================");
+ArraySelectorSolution.Run();
