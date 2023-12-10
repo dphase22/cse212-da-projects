6503480f5f4791e12a46ef01ef9726aa74c313dc
Demetrio-Aris <85774830+Demetrio-Aris@users.noreply.github.com>
2023-12-10 00:31:59 -0500

    Complete - W06 Code: Trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -8,29 +8,50 @@ public class Node {
     }
 
     public void Insert(int value) {
+        // TODO Start Problem 1
         if (value < Data) {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
-            else
+            else if (value != Left.Data)
                 Left.Insert(value);
-        }
-        else {
+
+        } else if (value > Data) { 
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
-            else
+            else if (value != Right.Data) 
                 Right.Insert(value);
         }
     }
 
     public bool Contains(int value) {
         // TODO Start Problem 2
-        return false;
+        if (value < Data) {
+            // Search in the left subtree
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+
+        } else if (value > Data) {
+            // Search in the right subtree
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        } else {
+            // Value found
+            return true;
+        }
     }
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight);
+        //return 0;  Replace this line with the correct return statement(s)
     }
 }
